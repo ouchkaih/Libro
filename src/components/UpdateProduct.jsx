0ccb3b975322e855021f8b9e2ef6e961fd42a757
ouchkaih smail <ouchkaihsmail@gmail.com>
2023-01-06 18:16:00 +0100

    update function work's

--- src/components/UpdateProduct.jsx
+++ src/components/UpdateProduct.jsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react'
-import {useLocation, useParams} from 'react-router-dom'
+import {useLocation, useNavigate, useParams} from 'react-router-dom'
 import axios from "axios"
 
 function UpdateProduct() {
+    // use useLocation to get the data that we passed from the productsManagment component using Navigate 
     const { state } = useLocation();
-    const [product, setProduct]= useState({image_url:"", title:"", description:"", price:""})
-
+    const [product, setProduct]= useState({image_url:state.img_url, title:state.title, description:state.description, price:state.price})
+    const navigate = useNavigate()
     // get product details
     const handlChange =(e)=>{
         setProduct(oldValue=>({
@@ -15,17 +16,21 @@ function UpdateProduct() {
     }
 
     // send data to php update file 
-    const Update = ()=>{
+    const Update = (e)=>{
+        e.preventDefault()
         const formdata = new FormData()
+        formdata.append("idp", state.id);
         formdata.append("image_url", product.image_url);
         formdata.append("title", product.title);
         formdata.append("description", product.description);
         formdata.append("price", product.price);
-        axios.post("",formdata).then(
-            (res)=>{
-
-            }
-        )
+        console.log(formdata)
+        axios
+          .post("http://localhost/php/w3ista/updateProduct.php", formdata)
+          .then((res) => {
+            alert(res.data.statut);
+            navigate("/managment");
+          });
     }
 
   return (
@@ -37,13 +42,13 @@ function UpdateProduct() {
                         <h4 className='text-center text-primary'>Create New Product</h4>
                     </div>
                     <label htmlFor="" className="form-label">Image Url</label>
-                    <input type="text" className="form-control" onChange={handlChange} defaultValue={state.image_url} name="image_url" placeholder="https://...." />
+                    <input type="text" className="form-control" onChange={handlChange} value={product.image_url} name="image_url" placeholder="https://...." />
                     <label htmlFor="" className="form-label mt-3">Title </label>
-                    <input type="text" className="form-control" onChange={handlChange} name='title' defaultValue={state.title} Placeholder='Product title' />
+                    <input type="text" className="form-control" onChange={handlChange} name='title' value={product.title} Placeholder='Product title' />
                     <label htmlFor="" className="form-label mt-3">Description</label>
-                    <textarea name="description" id="" cols="30" rows="10"  Placeholder="" className="form-control" defaultValue={state.description} onChange={handlChange}></textarea>
+                    <textarea name="description" id="" cols="30" rows="10"  Placeholder="" className="form-control" value={product.description} onChange={handlChange}></textarea>
                     <label htmlFor="" className="form-label mt-3">Price</label>
-                    <input type="text" className="form-control" onChange={handlChange} name="price" defaultValue={state.price} placeholder='00.00 Dhs' />
+                    <input type="text" className="form-control" onChange={handlChange} name="price" value={product.price} placeholder='00.00 Dhs' />
                     <button className="btn btn-primary mt-4 w-100" onClick={Update}>Create product</button>
                 </div>
             </form>
