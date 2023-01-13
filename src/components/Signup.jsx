4e62b4075b16dc1c9e60841646d8dfa41b63fd89
ouchkaih smail <ouchkaihsmail@gmail.com>
2023-01-13 21:16:56 +0100

    add email format validation

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -18,6 +18,7 @@ function Signup() {
         ))
     }
     
+    const [emailError, setEmailError] = useState("")
     let navigate = useNavigate()
     let dispatch = useDispatch()
 
@@ -35,29 +36,41 @@ function Signup() {
     const createAccount = (e)=>{
         e.preventDefault()
         if(user.userName !== "" &&user.userEmail !== "" &&user.userPassword !== ""  ){
-            if(!users.filter(items=>items.email === user.userEmail).length){
-              let formdata = new FormData();
-              formdata.append("username", user.userName);
-              formdata.append("email", user.userEmail);
-              formdata.append("password", user.userPassword);
-              axios
-                .post("http://localhost/php/w3ista/", formdata)
-                .then((res) => {
-                  if(res.data.isConnected === true){
-                    alert("Your Account was Created successfuly 😊 lets sign-in ")
-                    navigate("/Signin");
-
-                  }else{
-                    setError("something wrongs please tray to signup leater !");
-                  }
-                });
-              // dispatch(setConnection(true));
-              // dispatch(setUserName(user.userName));
-              // dispatch(setUserId(user.id));
-              // navigate("/products");
-            }else{
-              setError("this email already exist !");
-            }
+           let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+           if(re.test(user.userEmail)){
+            setEmailError("")
+             if (
+               !users.filter((items) => items.email === user.userEmail).length
+             ) {
+               let formdata = new FormData();
+               formdata.append("username", user.userName);
+               formdata.append("email", user.userEmail);
+               formdata.append("password", user.userPassword);
+               axios
+                 .post("http://localhost/php/w3ista/", formdata)
+                 .then((res) => {
+                  console.log(res.data.isConnected)
+                   if (res.data.isConnected === true) {
+                     alert(
+                       "Your Account was Created successfuly 😊 lets sign-in "
+                     );
+                     navigate("/Signin");
+                   } else {
+                     setError(
+                       "something wrongs please tray to signup leater !"
+                     );
+                   }
+                 });
+               // dispatch(setConnection(true));
+               // dispatch(setUserName(user.userName));
+               // dispatch(setUserId(user.id));
+               // navigate("/products");
+             } else {
+               setError("this email already exist !");
+             }
+           }else{
+              setEmailError("this email is not Valide Please enter another email ")
+           }
         }else{
           setError("Please fill out the form befor submited !")
         }
@@ -76,6 +89,9 @@ function Signup() {
                 <input type="text" className="form-control" name='userName' onChange={handlChange} placeholder='Ex: Jack' />
                 <label htmlFor="" className='form-label mt-3'>Email: </label>
                 <input type="email" className="form-control" name='userEmail' onChange={handlChange} placeholder='Ex: Jack@exmaple.com' />
+                <div className="p-0 m-0">
+                  <span className="text-danger">{emailError}</span>
+                </div>
                 <label htmlFor="" className='form-label mt-3'>Password: </label>
                 <input type="password" className="form-control" name='userPassword' onChange={handlChange} placeholder='Enter at least 8 characters' />
                 <button className="btn btn-primary w-100 mt-4" onClick={createAccount}>Create account</button>
