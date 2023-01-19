1d35686bca3f89727f12e963486a9f6a12d7bc88
ouchkaih smail <ouchkaihsmail@gmail.com>
2023-01-20 00:27:31 +0100

    add title to home page

--- src/components/index/Home.jsx
+++ src/components/index/Home.jsx
@@ -10,8 +10,18 @@ function Home() {
   return (
     <div>
       <div>
-        <div className="p-5 bg_orange row w-100 m-0" >
-          
+        <div className="p-5 bg_orange row w-100 m-0">
+          <div className="col-4 pt-5 pb-5 p-3">
+            <h1 className="p-3 pt-5 pb-5 mb-5">
+              <b>
+                <span> Get Your</span> 
+                <div className="p-1 d-inline-block bg-light" style={{width:"140px"}}></div><br />
+                <span className="" style={{fontSize:"64px"}}> New Book</span> <br />
+                <div className="p-1 d-inline-block bg-light" style={{width:"100px"}}></div>
+                <span className=""> Collection</span>
+              </b>
+            </h1>
+          </div>
         </div>
         <div className="row bg-light me-1">
           <div className="text-center p-4"></div>
