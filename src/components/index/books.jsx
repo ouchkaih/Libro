ace07a83a8a29489b34a4212f66d5be2d22906b5
ouchkaih smail <ouchkaihsmail@gmail.com>
2023-02-04 23:26:46 +0100

    add filtering with evaluation Clients

--- src/components/index/books.jsx
+++ src/components/index/books.jsx
@@ -13,11 +13,16 @@ function Books() {
 
     const [range, setRange] = useState({ min: 0, max: 100 });
     const [selectLanguage, setSelectLanguage] = useState("ALL LANGUAGES")
+    const [starsNum , setStarsNum ] = useState(0)
 
   // handleChange select input
     const byCategory = (e) => {
       setSelectedOption(e.target.value);
     };
+
+    const SelectRantingStars = (e)=>{
+      setStarsNum(parseInt(e.target.value));
+    }
 
   // handleChange Language 
   const handlChangeLanguage = (e)=>{
@@ -29,26 +34,45 @@ function Books() {
     }, [selectLanguage]);
 
     useEffect(() => {
+      // if the user doesn't select the category of books we check if is language selected or not
       if (category === "ALL CATEGORY") {
-        // in the first load and if the use want to see all books
         if (selectLanguage === "ALL LANGUAGES") {
-          setFilteredData(allBooks);
-        } else {
+          // filter books with stars numbers 
           setFilteredData(
-            allBooks.filter((item) => item.language.toUpperCase() === selectLanguage)
+            allBooks.filter(
+              (item) => item.rating >= starsNum
+            )
+          );
+        } else {
+          // filter the books with language and rating stars number
+          setFilteredData(
+            allBooks.filter(
+              (item) => item.language.toUpperCase() === selectLanguage && item.rating >= starsNum
+            )
           );
         }
       } else {
-        // whene the use filtred using select
+        // whene the user choose the category of book we check if is language is selected too
         if (selectLanguage === "ALL LANGUAGES") {
-          setFilteredData(allBooks.filter((item) => item.category ===category));
-        } else {
+          
+          // filter books with stars numbers and the category 
           setFilteredData(
-            allBooks.filter((item) => item.language.toUpperCase() === selectLanguage && item.category === category)
+            allBooks.filter(
+              (item) => item.rating >= starsNum && item.category === category
+            )
+          );
+        } else {
+          // selelct books with language, category and rating stars number 
+          setFilteredData(
+            allBooks.filter(
+              (item) =>
+                item.language.toUpperCase() === selectLanguage &&
+                item.category === category && item.rating >= starsNum
+            )
           );
         }
       }
-    }, [category, allBooks, selectLanguage]);
+    }, [category, allBooks, selectLanguage, starsNum]);
 
   return (
     <>
@@ -87,7 +111,7 @@ function Books() {
               <h6><b>Evaluation Clients</b></h6>
               <div>
                  <div className="selectStar">
-                  <input type="radio" name="star_number" id="fiveStar" className='' style={{width:"18px" , height:"18px" }} />
+                  <input type="radio" name="star_number" value={5} onChange={SelectRantingStars} id="fiveStar" className='' style={{width:"18px" , height:"18px" }} />
                  {/* stars icons  */}
                     <label htmlFor="fiveStar">
                       <AiFillStar style={{width:"25px" , height:"25px" , marginLeft:"8px"}} className="primary_color" />
@@ -98,7 +122,7 @@ function Books() {
                     </label>
                  </div>
                  <div className="selectStar">
-                  <input type="radio" name="star_number" id="fourStar" className='' style={{width:"18px" , height:"18px" }} />
+                  <input type="radio" name="star_number" value={4} onChange={SelectRantingStars} id="fourStar" className='' style={{width:"18px" , height:"18px" }} />
                  {/* stars icons  */}
                     <label htmlFor="fourStar">
                       <AiFillStar style={{width:"25px" , height:"25px" , marginLeft:"8px"}} className="primary_color" />
@@ -109,7 +133,7 @@ function Books() {
                     </label>
                  </div>
                  <div className="selectStar">
-                  <input type="radio" name="star_number" id="treeStar" className='' style={{width:"18px" , height:"18px" }} />
+                  <input type="radio" name="star_number" value={3} onChange={SelectRantingStars} id="treeStar" className='' style={{width:"18px" , height:"18px" }} />
                  {/* stars icons  */}
                     <label htmlFor="treeStar">
                       <AiFillStar style={{width:"25px" , height:"25px" , marginLeft:"8px"}} className="primary_color" />
@@ -120,7 +144,7 @@ function Books() {
                     </label>
                  </div>
                  <div className="selectStar">
-                  <input type="radio" name="star_number" id="twoStar" className='' style={{width:"18px" , height:"18px" }} />
+                  <input type="radio" name="star_number" value={2} onChange={SelectRantingStars} id="twoStar" className='' style={{width:"18px" , height:"18px" }} />
                  {/* stars icons  */}
                     <label htmlFor="twoStar">
                       <AiFillStar style={{width:"25px" , height:"25px" , marginLeft:"8px"}} className="primary_color" />
@@ -131,7 +155,7 @@ function Books() {
                     </label>
                  </div>
                  <div className="selectStar">
-                  <input type="radio" name="star_number" id="oneStar" className='' style={{width:"18px" , height:"18px" }} />
+                  <input type="radio" name="star_number" value={1} onChange={SelectRantingStars} id="oneStar" className='' style={{width:"18px" , height:"18px" }} />
                  {/* stars icons  */}
                     <label htmlFor="oneStar">
                       <AiFillStar style={{width:"25px" , height:"25px" , marginLeft:"8px"}} className="primary_color" />
